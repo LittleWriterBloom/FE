282958c9af0df807d5abd46a40989312886f7814
haesol822 <dordork@naver.com>
2024-04-12 11:40:46 +0900

    test: 프록시 나중에 수정, 다시 되돌리기

--- vite.config.ts
+++ vite.config.ts
@@ -8,4 +8,13 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://littlewriter-api.com/', // 프록시할 대상 URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // 요청 경로 재작성
+      },
+    },
+  },
 });
