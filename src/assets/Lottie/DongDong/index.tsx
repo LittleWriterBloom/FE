9fa0f64f2dc7b690f9ee2fca8835b264623c55e5
haesol822 <dordork@naver.com>
2024-05-03 11:34:32 +0900

    build: 로티에셋 관련 빌드 에러 수정 2차

--- src/assets/Lottie/DongDong/index.tsx
+++ src/assets/Lottie/DongDong/index.tsx
@@ -1,11 +1,11 @@
-import dongBase from './동동기본.json';
-import dongAppear from './동동등장.json';
-import dongTalk from './동동말하는.json';
-import dongRight from './동동오른쪽보는.json';
-import dongFront from './동동오른쪽에서정면으로.json';
-import dongHelp from './동동도움말말하는.json';
-import dongHelpStart from './동동도움말시작.json';
-import dongHelpEnd from './동동도움말끝.json';
+import dongBase from './dongBase.json';
+import dongAppear from './dongAppear.json';
+import dongTalk from './dongTalk.json';
+import dongRight from './dongRight.json';
+import dongFront from './dongFront.json';
+import dongHelp from './dongHelp.json';
+import dongHelpStart from './dongHelpStart.json';
+import dongHelpEnd from './dongHelpEnd.json';
 
 export {
   dongBase,
--- src/assets/Lottie/Ggummi/index.tsx
+++ src/assets/Lottie/Ggummi/index.tsx
@@ -1,9 +1,9 @@
-import ggummiBase from './꾸미기본.json';
-import ggummiAppear from './꾸미등장.json';
-import ggummiTalk from './꾸미말하는.json';
-import ggummiRight from './꾸미오른쪽보는.json';
-import ggummiFront from './꾸미오른쪽에서정면으로.json';
-import ggummiClap from './꾸미웃으며박수.json';
+import ggummiBase from './ggummiBase.json';
+import ggummiAppear from './ggummiAppear.json';
+import ggummiTalk from './ggummiTalk.json';
+import ggummiRight from './ggummiRight.json';
+import ggummiFront from './ggummiFront.json';
+import ggummiClap from './ggummiClap.json';
 
 export {
   ggummiBase,
